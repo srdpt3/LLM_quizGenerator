bb88e79b550df5f39c23730f092cd1bf9c0cbc64
Dustin Yang <srdpt3@gmail.com>
2024-09-28 23:25:20 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <Card className="w-[300px]">
         <CardHeader>
-          <CardTitle>Welcome to Quizzzy 🔥!</CardTitle>
+          <CardTitle>Welcome to 소은 AI 퀴즈🔥!</CardTitle>
           <CardDescription>
             Quizmi is a platform for creating quizzes using AI!. Get started by
             loggin in below!
